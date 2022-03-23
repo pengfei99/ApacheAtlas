bff2443ca72f0caf1a745e2c84dbfb0e9cac959f
pengfei <liu.pengfei@hotmail.fr>
2022-03-23 14:12:42 +0000

    add new search entity example

--- rest_api/search_entities/command.sh
+++ rest_api/search_entities/command.sh
@@ -12,8 +12,14 @@ curl -H "Authorization: Bearer ${token}" \
 -X POST -H "Content-Type: application/json" -H "Accept: application/json" "http://localhost:21000/api/atlas/v2/search/basic" \
 -d "@./search.json"
 
+# search a entity with type and attribute
+curl -H "Authorization: Bearer ${token}" \ -X GET "https://atlas.lab.sspcloud.fr/api/atlas/v2/search/attribute?attrName=qualifiedName&attrValuePrefix=user-pengfei%40movies.Character&limit=10&offset=0&typeName=hive_table" -H  "accept: application/json"
 
 
+
+curl -H "Authorization: Bearer ${token}" \
+-X POST -H 'Content-Type: application/json' -H 'Accept: application/json'  "https://atlas.lab.sspcloud.fr/api/atlas/v2/search/basic" \
+-d "@./search_hive_table.json"
 
 export token=
 
